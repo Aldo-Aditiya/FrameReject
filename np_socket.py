148258a02a41a89afdc155b4ad1267c38c6349d4
Aldo Aditiya <aldoan100@gmail.com>
2022-04-14 23:57:48 +0800

    Initial client-server socket code (not verified working)

---
+++ np_socket.py
@@ -0,0 +1,48 @@
+import socket
+import numpy as np
+import pickle
+
+class NPSocket():
+    def __init__(self):
+        pass
+
+    def start_server(self, port):
+        self.server_socket = socket.socket() 
+        self.server_socket.bind(('0.0.0.0', port))
+        self.server_socket.listen(1)
+        
+        print('Waiting for a connection...')
+        self.client_connection, client_address = self.server_socket.accept()
+        print(f'Connected to {client_address[0]} \n')
+
+    def start_client(self, server_address, port):
+        self.client_socket = socket.socket()
+        try:
+            self.client_socket.connect((server_address, port))
+            print(f'Connected to {server_address} on port {port} \n')
+        except socket.error, e:
+            print(f'Connection to {server_address} on port {port} failed: {e} \n')
+    
+    def send_arr(self, arr):
+        serialized = pickle.dumps(arr, protocol=2)
+        self.client_socket.sendall(serialize)
+        print 'Arr Sent'
+        
+    def receive_arr(self):
+        data = b''
+        while True:
+            receiving_buffer = self.client_connection.recv(1024)
+            if not receiving_buffer: break
+            data += receiving_buffer
+            print('-'),
+        out = pickle.loads(data)
+        print 'Arr Received'
+        return out
+    
+    def close_server(self):
+        self.client_connection.close()
+        self.server_socket.close()
+        
+    def close_client(self):
+        self.client_socket.shutdown(1)
+        self.client_socket.close()
---
+++ test_np_socket.py
@@ -0,0 +1,31 @@
+import sys
+import argparse
+import numpy as np
+from np_socket import NPSocket
+
+parser = argparse.ArgumentParser(description='')
+
+parser.add_argument('--server', help='Call if Server, Do not Call if Client', dest='server_store', action=argparse.BooleanOptionalAction)
+parser.add_argument('--server_address', help='Server IP Address', dest='server_address')
+parser.add_argument('--port', help='Socket Port', dest='port')
+
+FLAGS = parser.parse_args()
+
+np_socket = NPSocket()
+
+if FLAGS.server:
+    np_socket.start_server(FLAGS.port)
+    data = np_socket.receive_arr()
+    
+    print(data)
+    np_socket.close_server()
+    
+else:
+    np_socket.start_client(FLASG.server_address, FLAGS.port)
+    
+    data = np.zeros((5,5))
+    np_socket.send_arr(data)
+    
+    np_socket.close_client
+
+sys.exit()

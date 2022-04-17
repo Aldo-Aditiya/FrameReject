abbe827feef6fb70202881a9a512b1b4a806596d
Aldo Aditiya <aldoan100@gmail.com>
2022-04-17 21:48:52 +0800

    Completely overhauled Socket processing - from Thread to Multiprocesses. This is done to solve the GIL issue with Threading.

--- base_game_client.py
+++ base_game_client.py
@@ -1,9 +1,8 @@
 import time
 import argparse
 import numpy as np
-import threading
-from threading import Thread
-from queue import Queue
+from multiprocessing import Process
+from multiprocessing import Queue
 
 import pygame
 from game_socket.game_socket import GameClientSocket
@@ -14,37 +13,37 @@ parser = argparse.ArgumentParser(description='')
 parser.add_argument('--server_address', type=str, help='Server IP Address', dest='server_address')
 parser.add_argument('--main_port', type=int, help='Socket Port for Main Loop', dest='main_port')
 parser.add_argument('--input_port', type=int, help='Socket Port for Input Loop', dest='input_port')
+parser.add_argument('--frame_delay_ms', default=0, type=int, help='ms Delay in frame sending', dest='frame_delay_ms')
 
 FLAGS = parser.parse_args()
 
 # Threaded Input Function
-class ClientFrameReceiver(Thread):
+class ClientFrameReceiver(Process):
     def __init__(self, q, server_address, input_port):
-        Thread.__init__(self)
+        Process.__init__(self)
         self.daemon = True
-        self._stop_event = threading.Event()
+        self.stopped = False
         self.q = q
         self.input_port = input_port
         self.server_address = server_address
-        self.start()
 
     def run(self):
         self.cl_socket = GameClientSocket()
         self.cl_socket.start_client(self.server_address, self.input_port)
-        
-        while not self.stopped():
+
+        while not self.stopped:
             try:
+                time.sleep(FLAGS.frame_delay_ms / 1000)
                 data = self.cl_socket.client_receive_arr(decode=False)
                 self.q.put(data)
             except:
                 pass
         
     def stop(self):
-        self._stop_event.set()
-        self.cl_socket.close_client()
-        
-    def stopped(self):
-        return self._stop_event.is_set()   
+        self.stopped = True
+        #self.cl_socket.close_client()
+        self.join() 
+        self.close()
 
 def get_pygame_keypress():
     events = pygame.event.get()
@@ -60,57 +59,64 @@ def get_pygame_keypress():
     
     return num
 
-# PyGame Initialization
-pygame.init()
-screen = pygame.display.set_mode((160, 210))
+if __name__ == "__main__":
 
-# Initialize Socket
-q = Queue()
-t_cfr = ClientFrameReceiver(q, FLAGS.server_address, FLAGS.input_port)
-main_socket = GameClientSocket()
-main_socket.start_client(FLAGS.server_address, FLAGS.main_port)
+    # PyGame Initialization
+    pygame.init()
+    screen = pygame.display.set_mode((160, 210))
 
-# Main Loop
-is_game_over = False
-frame_time = 1 / 60
-time_frame = []
-keypress = 0
+    # Initialize Socket
+    q = Queue()
+    main_socket = GameClientSocket()
+    main_socket.start_client(FLAGS.server_address, FLAGS.main_port)
+    p_cfr = ClientFrameReceiver(q, FLAGS.server_address, FLAGS.input_port)
 
-frame_starttime = time.time()
+    # Main Loop
+    p_cfr.start()
+    is_game_over = False
+    frame_time = 1 / 60
+    time_frame = []
+    keypress = 0
 
-while not is_game_over:
-    # Get Player Keypress
-    if keypress == 0: keypress = get_pygame_keypress()
+    frame_starttime = time.time()
 
-    if ((time.time() - frame_starttime) >= frame_time):
-        time_frame.append(time.time() - frame_starttime)
-        frame_starttime = time.time()
+    while not is_game_over:
+        # Get Player Keypress
+        if keypress == 0: keypress = get_pygame_keypress()
 
-        # Send Last Keypress and reset
-        main_socket.client_send_int(keypress)
-        keypress = 0
+        running_frame_time = (time.time() - frame_starttime)
+        if (running_frame_time >= frame_time):
+            time_frame.append(running_frame_time)
+            frame_starttime = time.time()
 
-        if q.empty():
-            # If frame queue is empty, no need to process the current tick
-            pass
-        else:
-            # Get Frame Data from Queue
-            data = q.get()
-            data = main_socket.decode_arr(data)
+            # Send Last Keypress and reset
+            try:
+                main_socket.client_send_int(keypress)
+            except:
+                pass
+            keypress = 0
 
-            # Check if Loop Over (based on the shape of our data)
-            # TODO - Can be made better
-            if (data.shape[0] != 160):
-                is_game_over = True
-                break
-            
-            # Display Frame
-            frame = pygame.surfarray.make_surface(data)
-            screen.blit(frame, (0, 0))
-            pygame.display.update()
+            if q.empty():
+                # If frame queue is empty, no need to process the current tick
+                pass
+            else:
+                # Get Frame Data from Queue
+                data = q.get()
 
-t_cfr.stop()
-main_socket.close_client()
+                data = main_socket.decode_arr(data)
 
-mean_time_frame = np.mean(np.array(time_frame))
-print("\nClient End to End Process: " + str(mean_time_frame) + "s, or " + str(1/mean_time_frame) + " FPS")
+                # Check if Loop Over (based on the shape of our data)
+                # TODO - Can be made better
+                if (data.shape[0] != 160):
+                    is_game_over = True
+                    break
+                
+                # Display Frame
+                frame = pygame.surfarray.make_surface(data)
+                screen.blit(frame, (0, 0))
+                pygame.display.update()
+
+    main_socket.close_client()
+
+    mean_time_frame = np.mean(np.array(time_frame))
+    print("\nClient End to End Process: " + str(mean_time_frame) + "s, or " + str(1/mean_time_frame) + " FPS")
--- base_game_server.py
+++ base_game_server.py
@@ -1,9 +1,8 @@
 import time
 import argparse
 import numpy as np
-import threading
-from threading import Thread
-from queue import Queue
+from multiprocessing import Process
+from multiprocessing import Queue
 
 from ale_py import ALEInterface
 from game_socket.game_socket import GameServerSocket
@@ -17,20 +16,19 @@ parser.add_argument('--input_port', type=int, help='Socket Port for Input Loop',
 FLAGS = parser.parse_args()
 
 # Threaded Input Functions
-class ServerFrameSender(Thread):
+class ServerFrameSender(Process):
     def __init__(self, q, input_port):
-        Thread.__init__(self)
+        Process.__init__(self)
         self.daemon = True
-        self._stop_event = threading.Event()
+        self.stopped = False
         self.q = q
         self.input_port = input_port
-        self.start()
     
     def run(self):
         self.s_socket = GameServerSocket()
         self.s_socket.start_server(self.input_port)
-        
-        while not self.stopped():
+
+        while not self.stopped:
             if self.q.empty():
                 pass
             else:
@@ -38,80 +36,84 @@ class ServerFrameSender(Thread):
                 self.s_socket.server_send_arr(arr, encode=False)
         
     def stop(self):
-        self._stop_event.set()
-        self.s_socket.close_server()
+        self.stopped = True
+        #self.s_socket.close_server()
+        self.join()
+        self.close()
+
+if __name__ == "__main__":
+    # ALE Interface Initialization
+    ale = ALEInterface()
+    ale.setInt("random_seed", 42)
+    ale.setFloat("repeat_action_probability", 0)
+
+    rom_file = "./rom/breakout.bin"
+    ale.loadROM(rom_file)
+
+    minimal_actions = ale.getMinimalActionSet()
+    (screen_width,screen_height) = ale.getScreenDims()
+
+    # ALE Reset and Prep Screen and RAM Data
+    ram_size = ale.getRAMSize()
+    ram = np.zeros((ram_size),dtype=np.uint8)
+    ale.getRAM(ram)
+
+    (screen_width,screen_height) = ale.getScreenDims()
+    screen_data = np.zeros((screen_width,screen_height,3),dtype=np.uint8)
+    ale.getScreenRGB(screen_data)
+
+    (screen_width,screen_height) = ale.getScreenDims()
+    screen_data = np.zeros((screen_width,screen_height),dtype=np.uint8)
+    ale.getScreen(screen_data)
+
+    # Initialize Socket
+    q = Queue()
+    main_socket = GameServerSocket()
+    main_socket.start_server(FLAGS.main_port)
+    p_sfs = ServerFrameSender(q, FLAGS.input_port)
+
+    # Main Game Loop
+    p_sfs.start()
+    episode = 0
+    time_frame = []
+
+    keypress = 0
+    while (episode < 1):
+        print(f"\nEpisode: {episode}")
+        while not ale.game_over():
+            start_time = time.time()
+
+            # Wait for Client Input
+            try:
+                keypress = main_socket.server_receive_int()
+            except:
+                pass
+            a = minimal_actions[keypress]
+
+            # ALE Act
+            reward = ale.act(a);
+            
+            # Frame Generation
+            frame = ale.getScreenRGB()
+            frame = np.flip(np.rot90(frame), axis=0)
+            frame = main_socket.encode_arr(frame)
+            
+            # Send Frames
+            q.put(frame)
+
+            time_frame.append(time.time() - start_time)
+
+        episode += 1
+        ale.reset_game() 
+
+    # Indicate Loop Over with a Zero np Array
+    # TODO - Can be made better
+    loop_over = main_socket.encode_arr(np.zeros((5,5)))
+    q.put(loop_over)
+
+    time.sleep(1)
+
+    main_socket.close_server()
         
-    def stopped(self):
-        return self._stop_event.is_set()
-
-# ALE Interface Initialization
-ale = ALEInterface()
-ale.setInt("random_seed", 42)
-ale.setFloat("repeat_action_probability", 0)
-
-rom_file = "./rom/breakout.bin"
-ale.loadROM(rom_file)
-
-minimal_actions = ale.getMinimalActionSet()
-(screen_width,screen_height) = ale.getScreenDims()
-
-# ALE Reset and Prep Screen and RAM Data
-ram_size = ale.getRAMSize()
-ram = np.zeros((ram_size),dtype=np.uint8)
-ale.getRAM(ram)
-
-(screen_width,screen_height) = ale.getScreenDims()
-screen_data = np.zeros((screen_width,screen_height,3),dtype=np.uint8)
-ale.getScreenRGB(screen_data)
-
-(screen_width,screen_height) = ale.getScreenDims()
-screen_data = np.zeros((screen_width,screen_height),dtype=np.uint8)
-ale.getScreen(screen_data)
-
-# Initialize Socket
-q = Queue()
-t_sr = ServerFrameSender(q, FLAGS.input_port)
-main_socket = GameServerSocket()
-main_socket.start_server(FLAGS.main_port)
-
-# Main Game Loop
-episode = 0
-time_frame = []
-
-while (episode < 1):
-    print(f"\nEpisode: {episode}")
-    while not ale.game_over():
-        start_time = time.time()
-
-        # Wait for Client Input
-        keypress = main_socket.server_receive_int()
-        a = minimal_actions[keypress]
-
-        # ALE Act
-        reward = ale.act(a);
-        
-        # Frame Generation
-        frame = ale.getScreenRGB()
-        frame = np.flip(np.rot90(frame), axis=0)
-        frame = main_socket.encode_arr(frame)
-        
-        # Send Frames
-        q.put(frame)
-
-        time_frame.append(time.time() - start_time)
-
-    episode += 1
-    ale.reset_game() 
-
-# Indicate Loop Over with a Zero np Array
-# TODO - Can be made better
-loop_over = main_socket.encode_arr(np.zeros((5,5)))
-q.put(loop_over)
-
-time.sleep(1)
-
-t_sr.stop()
-main_socket.close_server()
-    
-mean_time_frame = np.mean(np.array(time_frame))
-print("\nServer End to End Process: " + str(mean_time_frame) + "s, or " + str(1/mean_time_frame) + " FPS")
+    mean_time_frame = np.mean(np.array(time_frame))
+    print("\nServer End to End Process: " + str(mean_time_frame) + "s, or " + str(1/mean_time_frame) + " FPS")
